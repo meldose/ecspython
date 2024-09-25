a3e30c204a9d3bd5a736d1c6a75e2ee867e0326d
midhun.eldose <midhun.eldose>
2024-09-25 16:25:42 +0200

    added few values

--- animationwith2cobots.py
+++ animationwith2cobots.py
@@ -13,8 +13,8 @@ bins = {"red": [5, 4], "blue": [5, 4], "green": [5, 2]}  # Sorting bins for Cobo
 object_position = [0, 0]   # Position of object on the conveyor
 
 # Different items (sorted by color)
-items = [ {"color": "red", "pos": [5, 1]},{"color":"blue","pos":[0,1]}]
-# {"color": "red", "pos": [0, 1]},
+items = [ {"color": "red", "pos": [5, 1]},{"color":"blue","pos":[0,1]},{"color": "green", "pos": [0, 1]},]
+
 item_speed = conveyor_speed
 
 # Create figure
