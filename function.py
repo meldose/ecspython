5d013c1582c367b213833a282ee5a64e600aa56e
Midhun <meldose@gmail.com>
2024-10-27 13:50:30 +0100

    added few lines

---
+++ function.py
@@ -0,0 +1,13 @@
+# Define a function
+def greet(name):
+    return f"Hello, {name}!"
+
+# Call the function
+result = greet("John")
+print(result)
+
+def greet(name):
+    print(f"Hello, {name}")
+    
+result= greet("Midhun")
+print(result)
